9fcd87b9870b359770e1e64307ba4809d7f31292
jaehong-kim <jaehong.kim@navercorp.com>
2015-12-30 16:44:35 +0900

    add license.

--- api/java/src/main/java/com/navercorp/redis/cluster/Pool.java
+++ api/java/src/main/java/com/navercorp/redis/cluster/Pool.java
@@ -1,10 +1,18 @@
 /*
- * @(#)Pool.java $version 2013. 8. 14.
+ * Copyright 2015 NAVER Corp.
  *
- * Copyright 2007 NHN Corp. All rights Reserved. 
- * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package com.navercorp.redis.cluster;
 
 import org.apache.commons.pool2.impl.GenericObjectPool;
