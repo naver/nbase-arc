f8b31c871a981b8116f5895e5b179de3b05af859
오준환 <rerun@naver.com>
2016-03-23 00:29:53 +0900

    Write to sample web and test case

---
+++ api/spring-boot/nbasearc-spring-boot-sample/src/main/java/com/navercorp/redis/cluster/spring/sample/HelloController.java
@@ -0,0 +1,39 @@
+package com.navercorp.redis.cluster.spring.sample;
+
+import com.navercorp.redis.cluster.spring.StringRedisClusterTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rerun on 2016. 3. 23..
+ */
+
+@RestController
+public class HelloController {
+
+    private static String KEY = "SAMPLE";
+    @Autowired
+    private StringRedisClusterTemplate redisTemplate;
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public Map<String, String> get() {
+        String sample = redisTemplate.opsForValue().get(KEY);
+        Map<String, String> re = new HashMap<String, String>();
+        re.put("retrun", sample);
+        return re;
+    }
+
+    @RequestMapping(value = "/put", method = RequestMethod.PUT)
+    @ResponseBody
+    public Map<String, String> put(@RequestBody Map<String, String> body) {
+        String value = body.get("insert");
+        redisTemplate.opsForValue().set(KEY, value);
+        Map<String, String> re = new HashMap<String, String>();
+        re.put("code", "200");
+        return re;
+    }
+}
---
+++ api/spring-boot/nbasearc-spring-boot-sample/src/main/java/com/navercorp/redis/cluster/spring/sample/SampleSimpleApplication.java
@@ -0,0 +1,16 @@
+package com.navercorp.redis.cluster.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rerun on 2016. 3. 23..
+ */
+
+@SpringBootApplication
+public class SampleSimpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSimpleApplication.class, args);
+
+    }
+}
