574f0d5790e7c44fad53973d110a6c3341a6a87b
developer-account <seunghoo.han@navercorp.com>
2018-02-08 14:38:00 +0900

    mgmt: in migration, add percentage-view for variation of key-count

--- tools/mgmt/show_info.py
+++ tools/mgmt/show_info.py
@@ -546,11 +546,15 @@ def show_key_variation(src_ip, src_redis_port, dst_ip, dst_redis_port, before_sr
         after_src_kcnt = redis_cmd.info_key_count()
     with RedisCmd(dst_ip, dst_redis_port) as redis_cmd:
         after_dst_kcnt = redis_cmd.info_key_count()
+    diff_src_kcnt = after_src_kcnt - before_src_kcnt
+    diff_dst_kcnt = after_dst_kcnt - before_dst_kcnt
     print yellow("+-------------------+---------------------------+---------------------------+")
     print yellow("|                   | SRC %-21s | DST %-21s |" % ("%s:%d" % (src_ip, src_redis_port), "%s:%d" % (dst_ip, dst_redis_port)))
     print yellow("+-------------------+---------------------------+---------------------------+")
     print yellow("| KEY COUNT(before) | %-25d | %-25d |" % (before_src_kcnt, before_dst_kcnt))
     print yellow("| KEY COUNT(after)  | %-25d | %-25d |" % (after_src_kcnt, after_dst_kcnt))
-    print yellow("| KEY COUNT(diff)   | %-25d | %-25d |" % (after_src_kcnt - before_src_kcnt, after_dst_kcnt - before_dst_kcnt ))
+    print yellow("| KEY COUNT(diff)   | %-25d | %-25d |" % (diff_src_kcnt, diff_dst_kcnt))
+    print yellow("| KEY COUNT(diff%%)  | %-25s | %-25s |" % ("%.2f%%" % (float(diff_src_kcnt) / (1 if before_src_kcnt == 0 else before_src_kcnt) * 100),
+                                                             "%.2f%%" % (float(diff_dst_kcnt) / (1 if before_dst_kcnt == 0 else before_dst_kcnt) * 100)))
     print yellow("+-------------------+---------------------------+---------------------------+\n")
 
